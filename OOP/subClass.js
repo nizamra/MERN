7f78de3a1e944c82e391670f51ebeba11bccf76a
nizamra <nizamra_95@hotmail.com>
2021-07-11 22:39:45 +0300

    sub class extends ninja

---
+++ OOP/subClass.js
@@ -0,0 +1,16 @@
+class Sensei extends Ninja{
+    constructor(z){
+        super(z);
+        this.health=200;
+        this.name=z;
+        this.wisdom=10;
+    }
+    speakWisdom(){
+        super.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.");
+    }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
